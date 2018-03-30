67001e023992b998a212c95a8922d9473da615ff
Madusha Prasanjith <mprasanjith@gmail.com>
2018-03-31 00:38:40 +0530

    Code cleanup.

---
+++ src/App.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const App = () => <div>All set for your next awesome project!</div>;
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,22 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Button } from 'antd';
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
 
-const title = "React Webpack Minimal";
-
-ReactDOM.render(<div>
-  {title}
-  <Button
-    type="primary"
-    onClick={() => { this.setState({ showing: 'cat' }); }}
-  >cat
-  </Button>
-  <Button
-    type="primary"
-    onClick={() => { this.setState({ showing: 'dog' }); }}
-  >dog
-  </Button>
-  </div>, document.getElementById("app"));
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("app")
+);
 
 if (process.env.NODE_ENV !== "production") {
   module.hot.accept();
